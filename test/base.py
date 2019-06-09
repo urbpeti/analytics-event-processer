4af7a8e2e852f394fc3bf1e5e90f57dc0c4b5ad1
Peter <urbpeti@gmail.com>
2019-06-09 12:15:56 +0200

    Setup base test case

---
+++ test/base.py
@@ -0,0 +1,10 @@
+from flask_testing import TestCase
+
+from app import app
+
+
+class BaseTestCase(TestCase):
+    """Base Tests"""
+
+    def create_app(self):
+        app.config.from_object('app.config.TestingConfig')
