d94555673f21a1353c08683226756c6f80a4cf93
Peter <urbpeti@gmail.com>
2019-06-09 12:09:13 +0200

    Add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from app.views.baseview import BaseView
 
 app = Flask(__name__)
+CORS(app)
 
 app_settings = os.getenv(
     'APP_SETTINGS',
